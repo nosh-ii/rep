60308b34bacdca3d0ae6e4ced41e791e928e653b
nosh-ii <108762120+nosh-ii@users.noreply.github.com>
2026-09-08 00:01:41 -0400

    Create repertoire.js

---
+++ repertoire.js
@@ -0,0 +1,22 @@
+const repertoire = [
+    {
+        instrument: "Clarinet",
+        name: "1st Movement from Concerto for Clarinet in A Major, K. 622",
+        composer: "W. A. Mozart",
+        part: ""
+    },
+
+    {
+        instrument: "Clarinet",
+        name: "1st Movement from Capriccio Espagnole, Op. 34",
+        composer: "Nikolai Rimsky-Korsakov",
+        part: ""
+    },
+
+    {
+        instrument: "Clarinet",
+        name: "3rd Movement from Capriccio Espagnole, Op. 34",
+        composer: "Nikolai Rimsky-Korsakov",
+        part: ""
+    }
+];
